b160b384daaf5c85173caf29b5612fde2e888528
Vismaya R <vismayar2811@gmail.com>
2023-08-22 23:30:55 +0530

    added beecolpy src code

--- api_abc.py
+++
@@ -1,6 +0,0 @@
-from abc.abc_problem import ABCProblem
-from util.functions import FUNCTIONS
-
-problem = ABCProblem(bees=10, function=FUNCTIONS['michalewicz'])
-best_bee = problem.solve()
-problem.replay()
