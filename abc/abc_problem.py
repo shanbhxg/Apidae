531b4c01de106615b73f43a576a6323b63ae5a26
Vismaya R <vismayar2811@gmail.com>
2023-08-22 17:19:56 +0530

    Util added

--- abc/abc_problem.py
+++ abc/abc_problem.py
@@ -1,7 +1,3 @@
-# ------------------------------------------------------------------------------------------------------
-#  Copyright (c) Leo Hanisch. All rights reserved.
-#  Licensed under the BSD 3-Clause License. See LICENSE.txt in the project root for license information.
-# ------------------------------------------------------------------------------------------------------
 
 from copy import deepcopy
 from functools import reduce
