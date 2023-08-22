40ae26ab8f0542c27d29f403058d66f7114665fe
Vismaya R <vismayar2811@gmail.com>
2023-08-22 22:06:28 +0530

    added API for hanisch code

---
+++ abc/api_abc.py
@@ -0,0 +1,6 @@
+from .abc_problem import ABCProblem
+from ..util.functions import FUNCTIONS
+
+problem = ABCProblem(bees=10, function=FUNCTIONS['michalewicz'])
+best_bee = problem.solve()
+problem.replay()
